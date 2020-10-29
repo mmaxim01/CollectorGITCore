786858e5d2dd39289400a7acdacc38fcc2687f6f
mmaxim01 <73255873+mmaxim01@users.noreply.github.com>
2020-10-29 23:48:07 +0100

    Update YML-gen.sh

--- configs/newcustomer/YML-gen.sh
+++ configs/newcustomer/YML-gen.sh
@@ -4,9 +4,9 @@ set -x
 # create /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NENAME/conflist.cfg like in the template folder 
 CUSTOMER=$1
 VERSION=$2
-
-
-for CONFFILE in `cat /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/conflist.cfg`; do
+$NE_TYPE=$3
+cp /srv/customers/hotstorage/configs/templates/configlist.cfg /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NE_TYPE/conflist.cfg 
+for CONFFILE in `cat /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NE_TYPE/conflist.cfg`; do
 echo "- pipeline.id: $NENAME-$CONFFILE" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
 echo "  path.config: \"/srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NENAME/$CONFFILE.conf\" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
 echo "  pipeline.workers: 1" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
@@ -15,3 +15,4 @@ echo "  pipeline.batch.delay: 20" >> /srv/customers/hotstorage/configs/$VERSION/
 echo "  queue.type: persisted" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
 done
 
+echo " remember to edit /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NE_TYPE/conflist.cfg"
