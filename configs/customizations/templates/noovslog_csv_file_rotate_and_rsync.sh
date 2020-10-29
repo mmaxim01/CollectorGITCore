27def41cc69eae026ad448f541d7204d3ea6c296
mmaxim01 <73255873+mmaxim01@users.noreply.github.com>
2020-10-29 22:13:53 +0100

    Update noovslog_csv_file_rotate_and_rsync.sh

--- configs/customizations/templates/noovslog_csv_file_rotate_and_rsync.sh
+++ configs/customizations/templates/noovslog_csv_file_rotate_and_rsync.sh
@@ -3,8 +3,8 @@ set -x
 #usage ./scriptname customer version
 customer=$1
 version=$2
-chmod -Rf 7777 /srv/customers/hotstorage/$version/$customer/*
-rsync -a /srv/customers/hotstorage/$version/$customer/tarred/*_csv.tar.gz /srv/customers/hotstorage/$version/$customer/archive/
-find /srv/customers/hotstorage/$version/$customer/ -type f -mtime +2 -exec rm -f {} \;
-rsync -a /srv/customers/hotstorge/$version/$customer/pm/ /srv/customers/hotstorage/$version/$customers/csvfolders
+chmod -Rf 7777 /srv/customers/hotstorage/customers/$version/$customer/*
+rsync -a /srv/customers/hotstorage/customers/$version/$customer/tarred/*_csv.tar.gz /srv/customers/hotstorage/customers/$version/$customer/archive/
+find /srv/customers/hotstorage/customers/$version/$customer/ -type f -mtime +2 -exec rm -f {} \;
+rsync -a /srv/customers/hotstorge/customers/$version/$customer/pm/ /srv/customers/hotstorage/customers/$version/$customers/csvfolders
 
