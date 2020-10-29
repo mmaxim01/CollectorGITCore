61685c410b074dbe4b96817b97564027bd82ea1b
mmaxim01 <73255873+mmaxim01@users.noreply.github.com>
2020-10-30 00:03:43 +0100

    Update YML-gen.sh

--- configs/newcustomer/YML-gen.sh
+++ configs/newcustomer/YML-gen.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 set -x 
-#usage ./scriptname newtworkelementnameassample customer version 
-# create /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NENAME/conflist.cfg like in the template folder 
+
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+  echo 'one or more variables are undefined'        
+  echo " usage ./scriptname customer version NE_TYPE "
+ exit 1
+fi
+
+
+echo " remember to edit /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NE_TYPE/conflist.cfg before execution of this script "
+echo "20 seconds left to cancel"
+
+sleep 30
+
+
 CUSTOMER=$1
 VERSION=$2
 $NE_TYPE=$3
+if [! -f /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NE_TYPE/conflist.cfg ]; then 
 cp /srv/customers/hotstorage/configs/templates/configlist.cfg /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NE_TYPE/conflist.cfg 
+echo " Going With Default Values found in /srv/customers/hotstorage/configs/templates/configlist.cfg "
+fi 
+
 for CONFFILE in `cat /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NE_TYPE/conflist.cfg`; do
 echo "- pipeline.id: $NENAME-$CONFFILE" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
 echo "  path.config: \"/srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NENAME/$CONFFILE.conf\" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
@@ -15,4 +31,4 @@ echo "  pipeline.batch.delay: 20" >> /srv/customers/hotstorage/configs/$VERSION/
 echo "  queue.type: persisted" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
 done
 
-echo " remember to edit /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NE_TYPE/conflist.cfg"
+
