1ecf060aef8371751e5fdd40b99dc948b0fe9126
mmaxim01 <73255873+mmaxim01@users.noreply.github.com>
2020-11-03 17:28:58 +0100

    Update customer-csv-processing.sh
    Updated

--- configs/customizations/templates/customer-csv-processing.sh
+++ configs/customizations/templates/customer-csv-processing.sh
@@ -1,14 +1,19 @@
-#!/bin/bash -  
+#!/bin/bash -
 set -x
 #title          :csv_processing.sh
-#description            
+#description
 #author         :Marian Maxim
 #date           :2019-09-20
 #version: 1     :4.4.20(1)-release
-version=$1 
+version=$1
 customer=$2
-filetountar=$(ls -lrtd /srv/customers/hotstorage/customers/$version/$customer/tarred/* | grep -v grep | grep -v mail | tail -1 | awk '{print $9}')
-cd /srv/customers/hotstorage/customers/$version/$customer/
+NetworkElementList=/srv/customers/hotstorage/configs/$version/$customer/NetworkElementList.cfg
+filetountar=$(ls -lrtd /srv/customers/hotstorage/customers/$version/$customer/tarred/pm/* | grep -v grep | grep -v mail | tail -1 | awk '{print $9}')
+
+find /srv/customers/hotstorage/customers/$version/$customer/pm/ -type f -name "*.csv" -mtime +22 -delete
+cd /srv/customers/hotstorage/customers/$version/$customer/tarred/pm/
+sync -a /srv/customers/hotstorage/customers/$version/$customer/tarred/pm/ /srv/customers/coldstorage/$version/$customer/tars/
 tar  -xvf $filetountar
-chmod -Rf 7777 /srv/customers/hotstorage/customers/$1/
-rsync -a /srv/customers/hotstorage/customers/$version/$customer/pm/ /srv/customers/hotstorage/customers/$version/$customer/csvfolders
+chmod -Rf 7777 /srv/customers/hotstorage/customers/$version/$customer/ && rm -Rf $filetountar
+rsync -a /srv/customers/hotstorage/customers/$version/$customer/tarred/pm/ /srv/customers/hotstorage/customers/$version/$customer/csvfolders
+
