000978320a402f100d7f93e08796180856eaf239
mmaxim01 <73255873+mmaxim01@users.noreply.github.com>
2021-06-04 18:24:53 +0200

    Update YML-gen.sh

--- configs/newcustomer/YML-gen.sh
+++ configs/newcustomer/YML-gen.sh
@@ -1,35 +1,16 @@
 #!/bin/bash
-set -x 
-
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-  echo 'one or more variables are undefined'        
-  echo " usage ./scriptname customer version NE_TYPE "
- exit 1
-fi
-
-
-echo " remember to edit /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NE_TYPE/conflist.cfg before execution of this script "
-echo "20 seconds left to cancel"
-
-sleep 30
-
-
-CUSTOMER=$1
-VERSION=$2
-$NE_TYPE=$3
-if [! -f /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NE_TYPE/conflist.cfg ]; then 
-echo "try cp /srv/customers/hotstorage/configs/templates/configlist.cfg /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NE_TYPE/conflist.cfg and edit the file with the needed csv filegroups"
-echo " exiting"
-exit 1
-fi 
-
-for CONFFILE in `cat /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NE_TYPE/conflist.cfg`; do
-echo "- pipeline.id: $NENAME-$CONFFILE" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
-echo "  path.config: \"/srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/$NENAME/$CONFFILE.conf\" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
-echo "  pipeline.workers: 1" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
-echo "  pipeline.batch.size: 60" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
-echo "  pipeline.batch.delay: 20" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
-echo "  queue.type: persisted" >> /srv/customers/hotstorage/configs/$VERSION/$CUSTOMER/pipelines.yml
+#set -x
+NENAME=$1
+CUSTOMER=$2
+VERSION=$3
+for CONFFILE in `cat /srv/configs/ver/$VERSION/$CUSTOMER/conflist.cfg`; do
+echo "- pipeline.id: $NENAME-$CONFFILE"
+echo "  path.config: \"/srv/configs/ver/$VERSION/$CUSTOMER/$NENAME/$CONFFILE.conf\" "
+echo "  pipeline.workers: 1"
+echo "  pipeline.batch.size: 2048"
+echo "  pipeline.batch.delay: 15"
+echo "  queue.type: persisted"
 done
+
 
 
